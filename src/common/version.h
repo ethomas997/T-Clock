cea31e5e7bd91f82fab0beed5fcde71953e8cc2c
E Thomas <ethomas997@etsphere.com>
2024-05-20 01:57:48 -0400

    Update version to 3.0.0 beta

--- src/common/version.h
+++ src/common/version.h
@@ -2,45 +2,45 @@
 #define AUTOVERSION_H
 /* Note: to use integer defines as strings, use STR(), eg. STR(VER_REVISION) */
 /**** Version ****/
-#	define VER_MAJOR 2
-#	define VER_MINOR 4
-#	define VER_BUILD 4
+#	define VER_MAJOR 3
+#	define VER_MINOR 0
+#	define VER_BUILD 0
 	/** status values: 0=Alpha(α), 1=Beta(β), 2=RC(гc), 3=Release(г), 4=Maintenance(гm) */
-#	define VER_STATUS 2
-#	define VER_STATUS_FULL "RC"
-#	define VER_STATUS_SHORT "rc"
-#	define VER_STATUS_GREEK "\u0433c"
-#	define VER_REVISION 492
-#	define VER_FULL "2.4.4 RC"
-#	define VER_SHORT "2.4rc4"
-#	define VER_SHORT_DOTS "2.4.4"
-#	define VER_SHORT_GREEK "2.4\u0433c4"
-#	define VER_RC_REVISION 2, 4, 4, 492
-#	define VER_RC_STATUS 2, 4, 4, 2
+#	define VER_STATUS 1
+#	define VER_STATUS_FULL "Beta"
+#	define VER_STATUS_SHORT "b"
+#	define VER_STATUS_GREEK "\u03B2"
+#	define VER_REVISION 531
+#	define VER_FULL "3.0.0 Beta"
+#	define VER_SHORT "3.0b0"
+#	define VER_SHORT_DOTS "3.0.0"
+#	define VER_SHORT_GREEK "3.0\u03B20"
+#	define VER_RC_REVISION 3, 0, 0, 531
+#	define VER_RC_STATUS 3, 0, 0, 1
 /**** Subversion Information ****/
-#	define VER_REVISION_URL "git@github.com:White-Tiger/T-Clock.git"
-#	define VER_REVISION_DATE "2018-01-28 19:22:27 +0000 (Sun, Jan 28 2018)"
-#	define VER_REVISION_HASH "e89dfdb"
-#	define VER_REVISION_TAG "v2.4.4#492-rc"
+#	define VER_REVISION_URL "https://github.com/ethomas997/T-Clock.git"
+#	define VER_REVISION_DATE "2018-06-08 08:42:06 +0000 (Fri, Jun 08 2018)"
+#	define VER_REVISION_HASH "2eb7e2a"
+#	define VER_REVISION_TAG "v3.0.0#531-beta"
 /**** Date/Time ****/
-#	define VER_TIMESTAMP 1517167741
-#	define VER_TIME_SEC 1
-#	define VER_TIME_MIN 29
-#	define VER_TIME_HOUR 19
-#	define VER_TIME_DAY 28
-#	define VER_TIME_MONTH 1
-#	define VER_TIME_YEAR 2018
-#	define VER_TIME_WDAY 0
-#	define VER_TIME_YDAY 27
-#	define VER_TIME_WDAY_SHORT "Sun"
-#	define VER_TIME_WDAY_FULL "Sunday"
-#	define VER_TIME_MONTH_SHORT "Jan"
-#	define VER_TIME_MONTH_FULL "January"
-#	define VER_TIME "19:29:01"
-#	define VER_DATE "2018-01-28"
-#	define VER_DATE_LONG "Sun, Jan 28, 2018 19:29:01 UTC"
-#	define VER_DATE_SHORT "2018-01-28 19:29:01 UTC"
-#	define VER_DATE_ISO "2018-01-28T19:29:01Z"
+#	define VER_TIMESTAMP 1716184533
+#	define VER_TIME_SEC 33
+#	define VER_TIME_MIN 55
+#	define VER_TIME_HOUR 5
+#	define VER_TIME_DAY 20
+#	define VER_TIME_MONTH 5
+#	define VER_TIME_YEAR 2024
+#	define VER_TIME_WDAY 1
+#	define VER_TIME_YDAY 140
+#	define VER_TIME_WDAY_SHORT "Mon"
+#	define VER_TIME_WDAY_FULL "Monday"
+#	define VER_TIME_MONTH_SHORT "May"
+#	define VER_TIME_MONTH_FULL "May"
+#	define VER_TIME "05:55:33"
+#	define VER_DATE "2024-05-20"
+#	define VER_DATE_LONG "Mon, May 20, 2024 05:55:33 UTC"
+#	define VER_DATE_SHORT "2024-05-20 05:55:33 UTC"
+#	define VER_DATE_ISO "2024-05-20T05:55:33Z"
 /**** Helper 'functions' ****/
 #	define VER_IsReleaseOrHigher() ( VER_STATUS >= 3 )
 #	define VER_IsAlpha() ( VER_STATUS == 0 )
